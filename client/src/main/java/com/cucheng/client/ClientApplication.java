b95d5aee8d37d92816edc285f5ed800f24be0d0e
cwm0103 <371396574@qq.com>
2019-09-20 21:19:43 +0800

    提交 client

---
+++ client/src/main/java/com/cucheng/client/ClientApplication.java
@@ -0,0 +1,15 @@
+package com.cucheng.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient //1.添加服务注册
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
